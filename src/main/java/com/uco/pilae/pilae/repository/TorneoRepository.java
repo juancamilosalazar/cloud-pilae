5720ad86fa8176f7b683adf1586bf8f0b64ba16a
camilo <juan.salazar@techandsolve.com>
2019-06-20 14:13:14 -0500

    CAMBIO JPArepository

--- src/main/java/com/uco/pilae/pilae/repository/TorneoRepository.java
+++ src/main/java/com/uco/pilae/pilae/repository/TorneoRepository.java
@@ -1,13 +1,14 @@
 package com.uco.pilae.pilae.repository;
 
 import com.uco.pilae.pilae.entity.TorneoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 @Repository
-public interface TorneoRepository extends CrudRepository<TorneoEntity,Long> {
+public interface TorneoRepository extends JpaRepository<TorneoEntity,Long> {
 
 
 }
